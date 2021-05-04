4f89aceb84d0d4bec260858b961871e5a1b2070d
m-rizwan-ashlar <m-rizwan@ashlarglobal.com>
2021-05-04 16:32:01 +0500

    add new button added

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -37,13 +37,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
+            <h1>Categories</h1>
           </div>
         </div>
       </div><!-- /.container-fluid -->
@@ -54,8 +48,8 @@
 
         <div class="card">
             <div class="card-header">
-              <h3 class="card-title">DataTable with default features</h3>
-              <a href="{{ route('category.create') }}" class="btn btn-success">Add New</a>
+              <h3 class="card-title">All Categories</h3>
+              <a href="{{ route('category.create') }}" class="btn btn-success offset-lg-4">Add New</a>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
